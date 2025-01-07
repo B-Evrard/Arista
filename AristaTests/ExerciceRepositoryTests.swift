61fff53c77cb88a1233a65990448a1acc5f12694
Bruno Evrard <brunoevrard@gmail.com>
2025-01-07 17:03:28 +0100

    Tests unitaires

--- AristaTests/ExerciceRepositoryTests.swift
+++ AristaTests/ExerciceRepositoryTests.swift
@@ -1,185 +1,213 @@
-////
-////  ExerciceRepositoryTests.swift
-////  AristaTests
-////
-////  Created by Bruno Evrard on 29/12/2024.
-////
 //
-//import XCTest
-//import CoreData
+//  ExerciceRepositoryTests.swift
+//  AristaTests
 //
-//final class ExerciceRepositoryTests: XCTestCase {
-//    
-//    private func emptyEntities(context: NSManagedObjectContext) {
-//        
-//        let fetchRequest = Exercise.fetchRequest()
-//        
-//        let objects = try! context.fetch(fetchRequest)
-//        
-//        
-//        
-//        for exercice in objects {
-//            
-//            context.delete(exercice)
-//            
-//        }
-//        
-//        
-//        
-//        try! context.save()
-//        
-//    }
-//    
-//    private func addExercice(context: NSManagedObjectContext, category: String, intensity: String, startDate: Date, endDate: Date,  userFirstName: String, userLastName: String) {
-//        
-//        let newUser = User(context: context)
-//        
-//        newUser.firstName = userFirstName
-//        
-//        newUser.lastName = userLastName
-//        
-//        try! context.save()
-//        
-//        
-//        
-//        let newExercise = Exercise(context: context)
-//        
-//        newExercise.type = category
-//        
-//        newExercise.intensity = intensity
-//        
-//        newExercise.startDate = startDate
-//        
-//        newExercise.endDate = endDate
-//        
-//        newExercise.user = newUser
-//        
-//        try! context.save()
-//        
-//    }
-//    
-//    func test_WhenNoExerciseIsInDatabase_GetExercise_ReturnEmptyList() {
-//        
-//        // Clean manually all data
-//        
-//        let persistenceController = PersistenceController(inMemory: true)
-//        
-//        emptyEntities(context: persistenceController.container.viewContext)
-//        
-//        let data = ExerciseRepository(viewContext: persistenceController.container.viewContext)
-//        
-//        let exercises = try! data.getExercise()
-//        
-//        XCTAssert(exercises.isEmpty == true)
-//        
-//    }
-//    
-//    func test_WhenAddingOneExerciseInDatabase_GetExercise_ReturnAListContainingTheExercise() {
-//        
-//        // Clean manually all data
-//        
-//        let persistenceController = PersistenceController(inMemory: true)
-//        
-//        emptyEntities(context: persistenceController.container.viewContext)
-//        
-//        let date = Date()
-//        
-//        addExercice(context: persistenceController.container.viewContext, category: "Football", duration: 10, intensity: 5, startDate: date, userFirstName: "Eric", userLastName: "Marcus")
-//        
-//        
-//        
-//        let data = ExerciseRepository(viewContext: persistenceController.container.viewContext)
-//        
-//        let exercises = try! data.getExercise()
-//        
-//        
-//        
-//        XCTAssert(exercises.isEmpty == false)
-//        
-//        XCTAssert(exercises.first?.category == "Football")
-//        
-//        XCTAssert(exercises.first?.duration == 10)
-//        
-//        XCTAssert(exercises.first?.intensity == 5)
-//        
-//        XCTAssert(exercises.first?.startDate == date)
-//        
-//    }
-//    
-//    func test_WhenAddingMultipleExerciseInDatabase_GetExercise_ReturnAListContainingTheExerciseInTheRightOrder() {
-//        
-//        // Clean manually all data
-//        
-//        let persistenceController = PersistenceController(inMemory: true)
-//        
-//        emptyEntities(context: persistenceController.container.viewContext)
-//        
-//        let date1 = Date()
-//        
-//        let date2 = Date(timeIntervalSinceNow: -(60*60*24))
-//        
-//        let date3 = Date(timeIntervalSinceNow: -(60*60*24*2))
-//        
-//        
-//        
-//        addExercice(context: persistenceController.container.viewContext,
-//                    
-//                    category: "Football",
-//                    
-//                    duration: 10,
-//                    
-//                    intensity: 5,
-//                    
-//                    startDate: date1,
-//                    
-//                    userFirstName: "Erica",
-//                    
-//                    userLastName: "Marcusi")
-//        
-//        addExercice(context: persistenceController.container.viewContext,
-//                    
-//                    category: "Running",
-//                    
-//                    duration: 120,
-//                    
-//                    intensity: 1,
-//                    
-//                    startDate: date3,
-//                    
-//                    userFirstName: "Erice",
-//                    
-//                    userLastName: "Marceau")
-//        
-//        addExercice(context: persistenceController.container.viewContext,
-//                    
-//                    category: "Fitness",
-//                    
-//                    duration: 30,
-//                    
-//                    intensity: 5,
-//                    
-//                    startDate: date2,
-//                    
-//                    userFirstName: "Frédericd",
-//                    
-//                    userLastName: "Marcus")
-//        
-//        
-//        
-//        let data = ExerciseRepository(viewContext: persistenceController.container.viewContext)
-//        
-//        let exercises = try! data.getExercise()
-//        
-//        
-//        
-//        XCTAssert(exercises.count == 3)
-//        
-//        XCTAssert(exercises[0].category == "Football")
-//        
-//        XCTAssert(exercises[1].category == "Fitness")
-//        
-//        XCTAssert(exercises[2].category == "Running")
-//        
-//    }
-//    
-//}
+//  Created by Bruno Evrard on 29/12/2024.
+//
+
+import XCTest
+import CoreData
+
+final class ExerciceRepositoryTests: XCTestCase {
+    
+    private func emptyEntities(context: NSManagedObjectContext) {
+        
+        let fetchRequest = Exercise.fetchRequest()
+        
+        let objects = try! context.fetch(fetchRequest)
+        
+        
+        
+        for exercice in objects {
+            
+            context.delete(exercice)
+            
+        }
+        
+        
+        
+        try! context.save()
+        
+    }
+    
+    private func addExercice(context: NSManagedObjectContext, category: String, intensity: Int, startDate: Date, endDate: Date,  userFirstName: String, userLastName: String, password: String) {
+        
+        let newUser = User(context: context)
+        
+        newUser.firstName = userFirstName
+        
+        newUser.lastName = userLastName
+        
+        newUser.password = password
+        
+        try! context.save()
+        
+        
+        
+        let newExercise = Exercise(context: context)
+        
+        newExercise.type = category
+        
+        newExercise.intensity = Int16(intensity)
+        
+        newExercise.startDate = startDate
+        
+        newExercise.endDate = endDate
+        
+        newExercise.user = newUser
+        
+        try! context.save()
+        
+    }
+    
+    func test_WhenNoExerciseIsInDatabase_GetExercise_ReturnEmptyList() {
+        
+        // Clean manually all data
+        
+        //let persistenceController = PersistenceController(inMemory: true)
+        
+        let persistenceController = PersistenceController.shared
+        
+        emptyEntities(context: persistenceController.container.viewContext)
+        
+        let data = ExerciseRepository(viewContext: persistenceController.container.viewContext)
+        
+        let exercises = try! data.getExercise()
+        
+        XCTAssert(exercises.isEmpty == true)
+        
+        // TODO: - Delete
+//        let dataUser = UserRepository(viewContext: persistenceController.container.viewContext)
+//        
+//        let user = try? dataUser.getUser()
+//        
+//        XCTAssert(user == nil)
+        
+    }
+    
+    func test_WhenAddingOneExerciseInDatabase_GetExercise_ReturnAListContainingTheExercise() {
+        
+        // Clean manually all data
+        
+        let persistenceController = PersistenceController(inMemory: true)
+        let entities = persistenceController.container.managedObjectModel.entities
+        print("Entities: \(entities.map { $0.name ?? "Unnamed" })")
+            
+        emptyEntities(context: persistenceController.container.viewContext)
+        
+        let date = Date()
+        let secondesAleatoires = Int.random(in: 3600...7200)
+        let endDate = date.addingTimeInterval(TimeInterval(secondesAleatoires))
+        
+        
+        addExercice(context: persistenceController.container.viewContext, category: "Football", intensity: 5, startDate: date, endDate: endDate, userFirstName: "Eric", userLastName: "Marcus", password: "motdepasseLong")
+
+        let data = ExerciseRepository(viewContext: persistenceController.container.viewContext)
+        
+        let exercises = try! data.getExercise()
+        
+        
+        
+        XCTAssert(exercises.isEmpty == false)
+        
+        XCTAssert(exercises.first?.type == "Football")
+        
+        XCTAssert(exercises.first?.intensity == 5)
+        
+        XCTAssert(exercises.first?.startDate == date)
+        
+        XCTAssert(exercises.first?.startDate == endDate)
+        
+    }
+    
+    func test_WhenAddingMultipleExerciseInDatabase_GetExercise_ReturnAListContainingTheExerciseInTheRightOrder() {
+        
+        // Clean manually all data
+        
+        let persistenceController = PersistenceController(inMemory: true)
+        let entities = persistenceController.container.managedObjectModel.entities
+            print("Entities: \(entities.map { $0.name ?? "Unnamed" })")
+            
+        
+        emptyEntities(context: persistenceController.container.viewContext)
+        
+        let date1 = Date()
+        
+        let date2 = Date(timeIntervalSinceNow: -(60*60*24))
+        
+        let date3 = Date(timeIntervalSinceNow: -(60*60*24*2))
+        
+        
+        
+        addExercice(context: persistenceController.container.viewContext,
+                    
+                    category: "Football",
+                    
+                    intensity: 5,
+                    
+                    startDate: date1,
+                    
+                    endDate: addRandomTime(to: date1),
+                    
+                    userFirstName: "Erica",
+                    
+                    userLastName: "Marcusi",
+        
+                    password: "motdepasseLong")
+        
+        addExercice(context: persistenceController.container.viewContext,
+                    
+                    category: "Running",
+                    
+                    intensity: 1,
+                    
+                    startDate: date3,
+                    
+                    endDate: addRandomTime(to: date3),
+                    
+                    userFirstName: "Erice",
+                    
+                    userLastName: "Marceau",
+                    
+                    password: "motdepasseLong")
+        
+        addExercice(context: persistenceController.container.viewContext,
+                    
+                    category: "Fitness",
+                    
+                    intensity: 5,
+                    
+                    startDate: date2,
+                    
+                    endDate: addRandomTime(to: date2),
+                    
+                    userFirstName: "Frédericd",
+                    
+                    userLastName: "Marcus",
+                    
+                    password: "motdepasseLong")
+        
+        
+        
+        let data = ExerciseRepository(viewContext: persistenceController.container.viewContext)
+        
+        let exercises = try! data.getExercise()
+        
+        
+        
+        XCTAssert(exercises.count == 3)
+        
+        XCTAssert(exercises[0].type == "Football")
+        
+        XCTAssert(exercises[1].type == "Fitness")
+        
+        XCTAssert(exercises[2].type == "Running")
+        
+    }
+    
+    private func addRandomTime(to: Date) -> Date {
+        let seconds = Int.random(in: 3600...7200)
+        return to.addingTimeInterval(TimeInterval(seconds))
+    }
+    
+}
